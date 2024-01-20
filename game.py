e2945f8afbb29571835dcfa72cbcc5bfdbdc4f2c
MainLaptop <jack.cumbers@outlook.com>
2024-01-20 20:12:28 +0000

    Created a basic game window

---
+++ game.py
@@ -0,0 +1,6 @@
+import pygame
+
+FPS = 60
+WIDTH, HEIGHT = 1280, 720
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("D&D Game (Temp Game Title)")
--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+from game import *
+import sys
+
+def draw_window():  # This is where the game is run
+    WINDOW.fill((69, 69, 69))
 
 if __name__ == '__main__':
-    print("Ayo!?")
+    clock = pygame.time.Clock()
+    while True:
+        clock.tick(FPS)
+        # Game Event Handler:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        draw_window()
+        pygame.display.update()
