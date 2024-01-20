d4525215215fcc4bd85a54d58fb7f1a9d0e265e5
MainLaptop <jack.cumbers@outlook.com>
2024-01-20 20:54:56 +0000

    - Added Window Resizing - Always Maintains a 16:9 Aspect Ratio

--- game.py
+++ game.py
@@ -1,6 +1,9 @@
 import pygame
 
 FPS = 60
-WIDTH, HEIGHT = 1280, 720
-WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("D&D Game (Temp Game Title)")
+
+class Game:
+    WIDTH = 1280
+    HEIGHT = 720
+    WINDOW = pygame.display.set_mode((WIDTH, HEIGHT), pygame.RESIZABLE)
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from game import *
 import sys
 
-def draw_window():  # This is where the game is run
-    WINDOW.fill((69, 69, 69))
+def draw_window():  # This is where the screen is rendered and the game is calculated
+    Game.WINDOW.fill((69, 69, 69))
 
 if __name__ == '__main__':
     clock = pygame.time.Clock()
@@ -10,8 +10,14 @@ if __name__ == '__main__':
         clock.tick(FPS)
         # Game Event Handler:
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT:  # X Button
                 pygame.quit()
                 sys.exit()
+            elif event.type == pygame.VIDEORESIZE:  # Resized Window - Maintains 16:9 Aspect Ratio
+                width = event.size[0]
+                height = round(width * 0.5625, 0)
+                Game.WINDOW = pygame.display.set_mode((width, height), pygame.RESIZABLE)
+                Game.WIDTH = width
+                Game.HEIGHT = height
         draw_window()
         pygame.display.update()
